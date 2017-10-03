14a64d80b0e2de7d44cad2af05b29f80245db3c5
Gabriel Melo Costa <mellogab83@gmail.com>
2017-10-02 22:44:05 -0700

    Adicionando Regra de Negócios de parte de Vendas
    Signed-off-by: Gabriel Melo Costa <mellogab83@gmail.com>

---
+++ sisCommerce/sisCommerce/Business/SaleBusiness.cs
@@ -0,0 +1,33 @@
+﻿using sisCommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sisCommerce.Business
+{
+    public class SaleBusiness
+    {
+        private SaleItems saleItems;
+        private SaleRepository saleRepository;
+
+        public SaleBusiness(SaleItems saleItems)
+        {
+            this.saleItems = saleItems;
+        }
+
+        public SaleBusiness()
+        {
+        }
+
+        public bool AddProductToShoppingCart()
+        {
+            /* criar view model que irá ter o item da venda(SaleItems) e os dados de produto(Products)
+             * necessário para validar quantidade disponível em estoque
+             * e calcular preço total a partir da quantidade desejada pelo cliente
+             */
+
+            return false;
+        }
+    }
+}
